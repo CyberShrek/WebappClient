e8d3e215ecb14a8503285fa6d523cf40bee172b5
Александр Ильин <ilin.aleksandr@vniizht.ru>
2025-09-02 20:23:01 +0300

    Initial commit

---
+++ src/main/typescript/webapp/model/App.ts
@@ -0,0 +1,18 @@
+interface AppInfo {
+    code: string
+    name?: string
+    groupName?: string
+    contextRoot?: string
+    groupPath?: string
+    version?: string
+    releaseDate?: string
+    updateDate?: string
+    technologistName?: string
+    technologistPhone?: string
+    technologistMail?: string
+    helpPath?: string
+    comment?: string
+    tables?: string[]
+    instructionPath?: string
+    description?: string
+}
